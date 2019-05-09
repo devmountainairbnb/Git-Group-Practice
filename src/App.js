a75b0b52a4a7638fef5e3a3fbab6398f416f1c23
Micaiah Buttars <michael.buttars@gmail.com>
2019-05-09 14:05:41 -0600

    TEST

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
               <h4>Signed in:</h4>
               <ul>
                 <li>Spencer Smith</li>
-                Micaiah Buttars
+                <li>Micaiah Buttars</li>
               </ul>
             </div>
           </div>
